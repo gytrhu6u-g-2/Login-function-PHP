567c779d3c05d1b5b3bf556498bde5186a06f001
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-05-30 10:35:47 +0900

    DB接続の設定

---
+++ dbconnect.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once './env.php';
+
+function connect() {
+    $host = DB_HOST;
+    $db = DB_NAME;
+    $user = DB_USER;
+    $pass = DB_PASS;
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass, [
+            //エラーを返す
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+            //配列をキーとバリューで返す
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+        echo '接続成功です';
+    } catch (PDOException $e) {
+        echo '接続失敗です' . $e->getMessage();
+        exit();
+    }
+}
+
+
+echo connect();
---
+++ env.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_NAME','user');
+define('DB_USER','gytrhu6u');
+define('DB_PASS','n2zdc@ec');
